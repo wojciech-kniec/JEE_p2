f9ef4921423ed4ce09e3012371b443b7e14ffc7d
rafalmisiak <rafalmisiak@gmail.com>
2019-04-10 13:42:05 +0200

    jee 1.1 workshops

--- UsersRepository.java
+++ UsersRepository.java
@@ -18,21 +18,21 @@ public class UsersRepository {
     private static void fillRepositoryWithDefaults() {
 
         User user1 = new User();
-        user1.setId(1);
+        user1.setId(1L);
         user1.setName("Jan");
         user1.setLogin("janko");
         user1.setAge(21);
         usersRepository.add(user1);
 
         User user2 = new User();
-        user2.setId(2);
+        user2.setId(2L);
         user2.setName("Adam");
         user2.setLogin("ano");
         user2.setAge(20);
         usersRepository.add(user2);
 
         User user3 = new User();
-        user3.setId(3);
+        user3.setId(3L);
         user3.setName("Anna");
         user3.setLogin("anmi");
         user3.setAge(20);
