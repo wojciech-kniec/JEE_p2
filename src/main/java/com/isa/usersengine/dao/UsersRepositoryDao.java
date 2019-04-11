6e2dd61f37150b278983d7371f5ff6245d46f84b
piokowalski <piokowalski@onet.pl>
2019-04-11 12:26:32 +0200

    UsersRepositoryDaoBean updated with methods from interface

--- src/main/java/com/isa/usersengine/dao/UsersRepositoryDao.java
+++ src/main/java/com/isa/usersengine/dao/UsersRepositoryDao.java
@@ -1,13 +1,14 @@
 package com.isa.usersengine.dao;
 
 import com.isa.usersengine.domain.User;
-import com.sun.tools.javac.util.List;
+
+import java.util.List;
 
 public interface UsersRepositoryDao {
 
     void addUser(User user);
 
-    void getUserByld(Long id);
+    User getUserById(Long id);
 
     User getUserByLogin(String login);
 
--- src/main/java/com/isa/usersengine/dao/UsersRepositoryDaoBean.java
+++ src/main/java/com/isa/usersengine/dao/UsersRepositoryDaoBean.java
@@ -1,9 +1,43 @@
 package com.isa.usersengine.dao;
 
 import com.isa.usersengine.domain.User;
-import com.sun.tools.javac.util.List;
+import com.isa.usersengine.repository.UsersRepository;
+
+import java.util.List;
 
 public class UsersRepositoryDaoBean implements UsersRepositoryDao {
 
+    @Override
+    public void addUser(User user) {
+        UsersRepository.getRepository().add(user);
+
+    }
+
+    @Override
+    public User getUserById(Long id) {
+
+        return UsersRepository
+                .getRepository()
+                .stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public User getUserByLogin(String login) {
+        return UsersRepository
+                .getRepository()
+                .stream()
+                .filter(user -> user.getLogin().equals(login))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<User> getUsersList() {
+        return UsersRepository
+                .getRepository();
     }
 }
+
