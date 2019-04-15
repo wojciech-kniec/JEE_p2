1b3a42cfa082d622db6c97d7fa59e5e41e3ade67
piokowalski <piokowalski@onet.pl>
2019-04-15 09:52:30 +0200

    First servlet created

--- src/main/java/com/isa/usersengine/servlet/HelloServlet.java
+++ src/main/java/com/isa/usersengine/servlet/HelloServlet.java
@@ -1,4 +1,19 @@
 package com.isa.usersengine.servlet;
 
-public class HelloServlet {
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/myFirstServlet")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void  doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.getWriter().println("Hello world from my first servlet");
+
+    }
 }
