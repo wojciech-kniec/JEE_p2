584167aae5dd04f9111556162729c3f2be539699
wojciech-kniec <kniecwojciech@gmail.com>
2019-04-15 10:44:00 +0200

    FindUserById servlet added

---
+++ src/main/java/com/isa/usersengine/servlet/FindUserByIdServlet.java
@@ -0,0 +1,8 @@
+package com.isa.usersengine.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/find-user-by-id")
+public class FindUserByIdServlet extends HttpServlet {
+}
